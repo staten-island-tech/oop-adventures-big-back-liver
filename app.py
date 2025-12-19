e4bf73f25e1e9e3b306876aa45ec65159e9f3d6f
olivia l <olivialu415@gmail.com>
2025-12-19 09:32:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ pygame.init()
 time = pygame.time.Clock()
 window = pygame.display.set_mode()
 running = True
-
+bg. 
 
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
+    
+
+pygame.quit()
