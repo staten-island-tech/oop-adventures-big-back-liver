ab78b7957799acb0fe038785338e0d4635d6b109
olivia l <olivialu415@gmail.com>
2025-12-19 09:37:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import pygame
 
 pygame.init()
 time = pygame.time.Clock()
-window = pygame.display.set_mode()
+window = pygame.display.set_mode((1280, 720))
 running = True
-bg. 
+bg = pygame.image.load('backgroundspookyforest.jpg') 
+bg = pygame.transform.scale(bg, (1280, 720))
 
 while running:
 
@@ -13,5 +14,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    window.blit(source= bg, dest= (0,0) )
+    pygame.display.flip()
+    time.tick(60)
 
 pygame.quit()
