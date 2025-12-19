863c1a7ce2f7317d24ee860ffadf79084ed39ed5
olivia l <olivialu415@gmail.com>
2025-12-19 09:42:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 import pygame 
+pygame.sprite.Group()
+
+class character(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+       
+       pygame.sprite.Sprite.__init__(self, )
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+       self.rect = self.image.get_rect()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 pygame.init()
