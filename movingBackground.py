262894e40630186600840e8c1a9e700fc96972c7
olivia l <olivialu415@gmail.com>
2025-12-22 09:46:26 -0500

    restartig

--- movingBackground.py
+++ movingBackground.py
@@ -1,8 +1,1 @@
-import pygame
-
-class backgrond():
-
-    def __init__(self, path = 'backgroundspookyforest.jpg'):
-        bg = pygame.image.load(path).convert()
-        bg = pygame.transform.scale(bg, (1280, 720))
-        
+ 
