0257d294a81e149991321ba17033624809172e22
olivia l <olivialu415@gmail.com>
2026-01-05 09:31:18 -0500

    okk

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
- 
+import tkinter
+from tkinter import *
+
+base = Tk()
+base.geometry("1500x900")
+base.resizable(False, False)
+
+base.mainloop()
--- movingBackground.py
+++ movingBackground.py
@@ -1,1 +1,11 @@
- 
+import tkinter
+from PIL import Image, ImageTk
+from tkinter import *
+
+class backgound(Frame):
+    
+    def __init__(self, masterx = None, imagex = "backgroundspookyforest.jpg" ):
+        super().__init__(master = masterx ,image = imagex)
+        self.piwolo = Image.open(imagex)
+        self.piwolo = self.piwolo.resize(size="1500x900")
+        self.config(imagex = self.piwolo)
