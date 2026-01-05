b63aebaf1cb0eff455b041aff771a5e0a590740a
olivia l <olivialu415@gmail.com>
2026-01-05 09:31:59 -0500

    hi

--- movingBackground.py
+++ movingBackground.py
@@ -6,6 +6,6 @@ class backgound(Frame):
     
     def __init__(self, masterx = None, imagex = "backgroundspookyforest.jpg" ):
         super().__init__(master = masterx ,image = imagex)
-        self.piwolo = Image.open(imagex)
-        self.piwolo = self.piwolo.resize(size="1500x900")
-        self.config(imagex = self.piwolo)
+        self.picter = Image.open(imagex)
+        self.picter = self.picter.resize(size="1500x900")
+        self.config(imagex = self.picter)
