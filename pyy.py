aaf4e1c83377ff0335f7dbf02a8873976189ccfd
ellaj19 <ellaj19@nycstudents.net>
2026-01-06 10:17:15 -0500

    Update pyy.py

--- pyy.py
+++ pyy.py
@@ -4,9 +4,11 @@ window = Tk()
 window.title("oop adventures")
 window.geometry("1500x900")
 window.resizable(False, False)
-my_button = Button(window, text="start game?")
-my_button.pack()
-if my_button == True:
-    my_response = Entry(window, width=30)
-    my_response.pack()
-    name = my_response
+label = Label(window, text="what's your name? type below", font=("Helvetica", 28), wraplength= 800)
+label.pack(pady=10)
+my_response = Entry(window, width=30, font=("Helvetica", 28))
+my_response.pack(pady=10)
+name = my_response
+my_button = Button(window, text="start game?", font=("Courier", 28), fg="white", bg="midnight blue", relief="ridge")
+my_button.pack(pady=10)
+window.mainloop()
