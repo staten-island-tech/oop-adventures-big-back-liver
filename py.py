522e9d0667c7da7f5502d9c9f1afe0b44f66e785
ellaj19 <ellaj19@nycstudents.net>
2026-01-07 10:17:49 -0500

    Update py.py

---
+++ py.py
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+=======
+import tkinter as tk
+window = tk.Tk()
+window.title("eat")
+window.geometry("200x200") 
+window.resizable(False, False)
+my_button = tk.Button(window, text = "", font=("Courier", 24), bg = "white", fg="black", relief = "raised", padx=10, pady = 5)
+window.mainloop()
+>>>>>>> 5d0b03d4e8f470ad7ef39783d84c6e7df4ed1487
