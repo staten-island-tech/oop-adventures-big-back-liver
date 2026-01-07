968944966e27ce755a8b96a824446829866c8c89
ellaj19 <ellaj19@nycstudents.net>
2026-01-07 10:05:42 -0500

    u8yu

--- pyy.py
+++ pyy.py
@@ -1,9 +1,14 @@
 import tkinter
 from tkinter import *
+from PIL import Image, ImageTk
 window = Tk()
 window.title("oop adventures")
 window.geometry("1500x900")
 window.resizable(False, False)
+
+FILe = Image.open("name.jpg")
+FILe = FILe.resize(size=[1500,900])
+picture = Label(master= window, image="")
 label = Label(window, text="what's your name? type below", font=("Helvetica", 28), wraplength= 800)
 label.pack(pady=10)
 my_response = Entry(window, width=30, font=("Helvetica", 28))
@@ -12,3 +17,4 @@ name = my_response
 my_button = Button(window, text="start game?", font=("Courier", 28), fg="white", bg="midnight blue", relief="ridge")
 my_button.pack(pady=10)
 window.mainloop()
+
