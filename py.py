8b12af3d6b221d4396e698ddbc5b720f7e631301
mial-92 <mialin0170@gmail.com>
2026-01-08 10:12:38 -0500

    Update py.py

--- py.py
+++ py.py
@@ -3,12 +3,16 @@ import tkinter
 from tkinter import *
 window = Tk()
 window.title("Button Example")
-my_button = Button(window, text="What would you like to eat?", font=("Arial", 16), bg="lightblue", fg="black", relief="raised", 
+my_button = Button(window, text="Eat with feet", font=("Arial", 16), bg="lightblue", fg="black", relief="raised", 
+padx=10, pady=5)
+my_button
+lable = Label(window, text="", wraplength= 800)
+lable.pack()
+window.mainloop()
+
+my_button2 = Button(window, text="Eat with hands", font=("Arial", 16), bg="lightblue", fg="black", relief="raised", 
 padx=10, pady=5)
 my_button.pack()
-
-resposnes = Entry(window, width= 30)
-resposnes.pack()
 
 lable = Label(window, text="", wraplength= 800)
 lable.pack()
