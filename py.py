2e7497a119516fd7a76ed9f4206eb9349dae0c6a
mial-92 <mialin0170@gmail.com>
2026-01-14 10:17:01 -0500

    dbnf

--- py.py
+++ py.py
@@ -10,7 +10,7 @@ window.resizable(False, False)
 my_button = Button(window, text="Eat with feet", font=("Arial", 16), bg="lightblue", fg="black", relief="raised", 
 padx=100, pady=50)
 my_button.pack()
-my_button2 = Button(window, text="Eat with hands", font=("Arial", 16), bg="lightblue", fg="black", relief="raised", 
+my_button2 = Button(window, text="stop eating", font=("Arial", 16), bg="lightblue", fg="black", relief="raised", 
 padx=10, pady=5)
 my_button2.pack()
 
