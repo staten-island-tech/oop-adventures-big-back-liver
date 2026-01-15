a52a1f81e11eb6be29b38503ede29b4f00658007
ellaj19 <ellaj19@nycstudents.net>
2026-01-15 10:17:18 -0500

    Update pyy.py

--- pyy.py
+++ pyy.py
@@ -11,7 +11,7 @@ FILe = FILe.resize(size=[900,900])
 FILe = ImageTk.PhotoImage(FILe)
 
 x = StringVar()
- 
+
 
 
 picture = Label(master= window, image=FILe)
@@ -25,17 +25,13 @@ my_button = Button(picture, text="start game?", font=("Courier", 28), fg="white"
 my_button.place(relx = .35 , rely = .35)
 
 window.wait_variable(x)
+my_button.config(command= lambda: my_button.destroy())
+my_button.destroy()
+
 FILe = Image.open("hm.png")
 FILe = FILe.resize(size=[900,900])
 FILe = ImageTk.PhotoImage(FILe)
 picture.config(image=FILe)
+buttontwo = Button(window, text="moisturize", font=("Courier", 28), fg="white", bg="forest green")
+buttontwo.place(relx = .5 , rely = .5)
 window.mainloop()
-""" 
-from app import base
-class backgroundgame(Frame):
-    def __init__(self, masterx = base, imagex = "hm.png"):
-        super().__init__(master = masterx ,image = imagex)
-        self.picter = Image.open(imagex)
-        self.picter = self.picter.resize(size="900x900")
-        self.config(imagex = self.picter)
- """
