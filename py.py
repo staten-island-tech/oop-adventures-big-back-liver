d15dbe57cf5cb1f2e20c6020f9cc74653f61b588
mial-92 <mialin0170@gmail.com>
2026-01-15 09:53:55 -0500

    Update py.py

--- py.py
+++ py.py
@@ -6,22 +6,21 @@ window = Tk()
 window.title("Button Example")
 window.geometry("900x900")
 window.resizable(False, False)
-
-my_button = Button(window, text="Eat with feet", font=("Arial", 16), bg="lightblue", fg="black", relief="raised", 
-padx=100, pady=50)
-my_button.pack()
-my_button.place()
-my_button2 = Button(window, text="stop eating", font=("Arial", 16), bg="lightblue", fg="black", relief="raised", 
-padx=10, pady=5)
-my_button2.pack()
-my_button2.place()
-
 image = Image.open("mukbang.png")
 image = image.resize(size=[900,900])
 image = ImageTk.PhotoImage(image)
 
 picture = Label(master= window, image=image)
 picture.pack()
+
+my_button = Button(window, text="Eat with feet", font=("Arial", 16), bg="aliceblue", fg="black", relief="raised", 
+padx=50, pady=15)
+my_button.place(relx = .4 , rely = .2)
+my_button2 = Button(window, text="stop eating", font=("Arial", 16), bg="lightsalmon", fg="black", relief="raised", 
+padx=10, pady=5)
+my_button2.place(relx = .4 , rely = .9)
+
+
 
 lable = Label(window, text="", wraplength= 800)
 lable.pack()
